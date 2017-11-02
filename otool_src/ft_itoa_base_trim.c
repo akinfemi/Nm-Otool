9b86fdfbcd69e94538e2beaffd02d8755ece0348
akinfemi <akinfemi.aluko@gmail.com>
2017-11-02 13:25:36 -0700

    norme

--- otool_src/ft_itoa_base_trim.c
+++ otool_src/ft_itoa_base_trim.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*   ft_itoa_base_trim.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aakin-al <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/17 21:17:57 by aakin-al          #+#    #+#             */
-/*   Updated: 2017/09/01 10:38:42 by aakin-al         ###   ########.fr       */
+/*   Created: 2017/11/02 13:17:45 by aakin-al          #+#    #+#             */
+/*   Updated: 2017/11/02 13:18:03 by aakin-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ char			*ft_itoa_base_trim(uintmax_t val, int base, int trim)
 	int			sign;
 
 	sign = 1;
-    len = trim;
+	len = trim;
 	str = (char *)malloc(sizeof(char) * len + 1);
-    str[len] = '\0';
+	str[len] = '\0';
 	while (len > 0)
 	{
-        if (val == 0)
-            str[--len] = '0';
-        else
-		    str[--len] = (val % base) + (val % base > 9 ? 'a' - 10 : '0');
-        val /= base;
+		if (val == 0)
+			str[--len] = '0';
+		else
+			str[--len] = (val % base) + (val % base > 9 ? 'a' - 10 : '0');
+		val /= base;
 	}
 	return (str);
 }
--- otool_src/util.c
+++ otool_src/util.c
@@ -1,53 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   util.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakin-al <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/02 13:19:02 by aakin-al          #+#    #+#             */
+/*   Updated: 2017/11/02 13:25:16 by aakin-al         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/otool.h"
-#include <stdio.h>
 
-
-void        parse_list(t_list *ar_list, char *ptr, char *lib_name)
+void		parse_list(t_list *ar_list, char *ptr, char *lib_name)
 {
-    struct  ar_hdr  *ar;
-    t_offset        *off;
-    int             name_off;
+	struct ar_hdr	*ar;
+	t_offset		*off;
+	int				name_off;
 
-    while (ar_list)
-    {
-        off = ar_list->content;
-        ar = (void *)ptr + off->offset;
-        name_off = ft_atoi(ft_strchr(ar->ar_name, '/') + 1);
-        ft_printf("%s(%s)\n", lib_name, off->name);
-        ft_otool((void *)ar + sizeof(struct ar_hdr) + name_off, off->name);
-        ar_list = ar_list->next;
-    }
+	while (ar_list)
+	{
+		off = ar_list->content;
+		ar = (void *)ptr + off->offset;
+		name_off = ft_atoi(ft_strchr(ar->ar_name, '/') + 1);
+		ft_printf("%s(%s)\n", lib_name, off->name);
+		ft_otool((void *)ar + sizeof(struct ar_hdr) + name_off, off->name);
+		ar_list = ar_list->next;
+	}
 }
 
-t_offset      *new_off_t_node(struct ranlib ran)
+t_offset	*new_off_t_node(struct ranlib ran)
 {
-    t_offset  *off_t_node;
+	t_offset	*off_t_node;
 
-    off_t_node = malloc(sizeof(t_offset));
-    off_t_node->name = NULL;
-    off_t_node->offset = ran.ran_off;
-    off_t_node->ran_strx = ran.ran_un.ran_strx;
-    return (off_t_node);
+	off_t_node = malloc(sizeof(t_offset));
+	off_t_node->name = NULL;
+	off_t_node->offset = ran.ran_off;
+	off_t_node->ran_strx = ran.ran_un.ran_strx;
+	return (off_t_node);
 }
 
-t_list         *make_list(struct ranlib ran, char *ptr)
+t_list		*make_list(struct ranlib ran, char *ptr)
 {
-    t_offset      *off_node;
-    struct ar_hdr  *ar;
+	t_offset		*off_node;
+	struct ar_hdr	*ar;
 
-    off_node = new_off_t_node(ran);
-    ar = (void *)ptr + off_node->offset;
-    off_node->name = ft_strdup(ft_strchr(ar->ar_name, '`') + 2);
-    off_node->make_time = ft_atomax(ar->ar_date);
-    return(ft_lstnew(off_node, sizeof(t_offset)));
+	off_node = new_off_t_node(ran);
+	ar = (void *)ptr + off_node->offset;
+	off_node->name = ft_strdup(ft_strchr(ar->ar_name, '`') + 2);
+	off_node->make_time = ft_atomax(ar->ar_date);
+	return (ft_lstnew(off_node, sizeof(t_offset)));
 }
 
-int				comp_alpha_two(void *p1, void *p2, int reverse)
+int			comp_alpha_two(void *p1, void *p2, int reverse)
 {
-    t_offset  *f1;
-    t_offset  *f2;
+	t_offset	*f1;
+	t_offset	*f2;
 
-    f1 = (t_offset *)p1;
+	f1 = (t_offset *)p1;
 	f2 = (t_offset *)p2;
 	if (!reverse)
 		return (f1->make_time > f2->make_time ? 1 : 0);
