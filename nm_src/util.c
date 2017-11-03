90b82644cf6cf10ba25ecd873dc22c442bca422c
akinfemi <akinfemi.aluko@gmail.com>
2017-11-03 00:03:24 -0700

    norme

--- nm_src/util.c
+++ nm_src/util.c
@@ -6,18 +6,33 @@
 /*   By: aakin-al <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/02 13:11:07 by aakin-al          #+#    #+#             */
-/*   Updated: 2017/11/02 22:36:54 by aakin-al         ###   ########.fr       */
+/*   Updated: 2017/11/03 00:02:49 by aakin-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/nm.h"
 
-char        get_type_o(t_nm_basic *object)
+static char		gt_o(t_nm_basic *object)
 {
-	t_type      type;
+	t_type		type;
 
 	type = object->type;
-	if (type.n_ext == 1 && type.n_type == 14 && type.n_pext == 16 && type.n_stab == 64)
+	if (type.n_ext == 0 && type.n_type == 0 && type.n_pext == 0
+			&& type.n_stab == 0)
+	{
+		if (object->sect == 1 && object->value == 0)
+			return ('T');
+	}
+	return ('\0');
+}
+
+char			get_type_o(t_nm_basic *object)
+{
+	t_type		type;
+
+	type = object->type;
+	if (type.n_ext == 1 && type.n_type == 14 && type.n_pext == 16
+			&& type.n_stab == 64)
 	{
 		if (object->sect == 0)
 			return ('U');
@@ -26,7 +41,7 @@ char        get_type_o(t_nm_basic *object)
 		if (object->value == 0)
 			return ('T');
 	}
-	if (type.n_stab == 96 )
+	if (type.n_stab == 96)
 	{
 		if (object->value != 0 && type.n_pext == 2)
 			return ('t');
@@ -34,20 +49,67 @@ char        get_type_o(t_nm_basic *object)
 			return ('D');
 		return ('T');
 	}
-	if (type.n_ext == 0 && type.n_type == 0 && type.n_pext == 0 && type.n_stab == 0)
+	return (gt_o(object));
+}
+
+static char		get_type_one(t_nm_basic *object)
+{
+	t_type		type;
+
+	type = object->type;
+	if ((type.n_ext == 0 && type.n_type == 8) ||
+			(type.n_ext == 1 && type.n_type == 4))
 	{
-		if (object->sect == 1 && object->value == 0)
+		if (object->sect == 9 || object->sect == 8)
+			return ('D');
+		return ('T');
+	}
+	if ((type.n_ext == 1 && type.n_type == 8 && object->sect == 1) ||
+			(object->sect == 1 && type.n_type == 4))
+		return ('t');
+	if (object->sect == 1 && type.n_type == 14 && type.n_ext == 0)
+		return ('T');
+	if ((object->sect == 4 || object->sect == 14 || object->sect == 6) &&
+			type.n_type == 4)
+		return ('s');
+	if (object->sect == 16 && type.n_type == 4)
+		return ('b');
+	return ('\0');
+}
+
+static char		get_type_two(t_nm_basic *object)
+{
+	t_type		type;
+
+	type = object->type;
+	if (type.n_ext == 0 && type.n_type == 0 && type.n_pext == 0 &&
+			type.n_stab == 0)
+	{
+		if (object->desc >= 256)
+			return ('U');
+	}
+	if (object->sect == 1 && type.n_ext == 1 && type.n_type == 2 &&
+			type.n_stab == 64)
+		return ('T');
+	if (object->sect == 15 && type.n_ext == 0 && type.n_type == 4 &&
+			type.n_stab == 96)
+		return ('d');
+	if (type.n_ext == 0 && type.n_type == 10 && type.n_stab == 64)
+	{
+		if (object->sect == 1)
 			return ('T');
+		return ('S');
 	}
 	return ('\0');
 }
 
-char        get_type(t_nm_basic *object)
+char			get_type(t_nm_basic *object)
 {
-	t_type      type;
+	t_type		type;
 
 	type = object->type;
-	if (type.n_ext == 1 && type.n_type == 14 && type.n_pext == 16 && type.n_stab == 64)
+	if (type.n_ext == 1 && type.n_type == 14 && type.n_pext == 16 &&
+			type.n_stab == 64)
 	{
 		if (object->sect == 1)
 			return ('t');
@@ -60,35 +122,7 @@ char        get_type(t_nm_basic *object)
 	}
 	if (type.n_pext == 0 && type.n_stab == 96)
 	{
-		if ((type.n_ext == 0 && type.n_type == 8) || (type.n_ext == 1 && type.n_type == 4))
-		{
-			if (object->sect == 9 || object->sect == 8)
-				return ('D');
-			return ('T');
-		}
-		if ((type.n_ext == 1 && type.n_type == 8 && object->sect == 1) || (object->sect == 1 && type.n_type == 4))
-			return ('t');
-		if (object->sect == 1 && type.n_type == 14 && type.n_ext == 0)
-			return ('T');
-		if ((object->sect == 4 || object->sect == 14 || object->sect == 6) && type.n_type == 4)
-			return ('s');
-		if (object->sect == 16 && type.n_type == 4)
-			return ('b');
+		return (get_type_one(object));
 	}
-	if (type.n_ext == 0 && type.n_type == 0 && type.n_pext == 0 && type.n_stab == 0)
-	{
-		if (object->desc >= 256)
-			return ('U');
-	}
-	if (object->sect == 1 && type.n_ext == 1 && type.n_type == 2 && type.n_stab == 64)
-		return ('T');
-	if (object->sect == 15 && type.n_ext == 0 && type.n_type == 4 && type.n_stab == 96)
-		return ('d');
-	if (type.n_ext == 0 && type.n_type == 10 && type.n_stab == 64)
-	{
-		if (object->sect == 1)
-			return ('T');
-		return ('S');
-	}
-	return ('\0');
+	return (get_type_two(object));
 }
