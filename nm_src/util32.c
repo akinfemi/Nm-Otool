4047579c4ea523a001041f3ed409fa56c1760b1e
akinfemi <akinfemi.aluko@gmail.com>
2017-11-02 23:45:02 -0700

    norme

--- nm_src/util32.c
+++ nm_src/util32.c
@@ -6,19 +6,19 @@
 /*   By: aakin-al <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/02 13:11:07 by aakin-al          #+#    #+#             */
-/*   Updated: 2017/11/02 22:36:54 by aakin-al         ###   ########.fr       */
+/*   Updated: 2017/11/02 23:44:41 by aakin-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/nm.h"
 
-void        build_list_32(struct symtab_command *sym, char *ptr, int obj)
+void		build_list_32(struct symtab_command *sym, char *ptr, int obj)
 {
-	uint32_t            i;
-	struct nlist        *arr;
-	char                *s_table;
-	t_list              *objects;
-	t_nm_basic          *object;
+	uint32_t			i;
+	struct nlist		*arr;
+	char				*s_table;
+	t_list				*objects;
+	t_nm_basic			*object;
 
 	i = 0;
 	s_table = (void *)ptr + sym->stroff;
@@ -40,16 +40,16 @@ void        build_list_32(struct symtab_command *sym, char *ptr, int obj)
 	print_list_32(objects, obj);
 }
 
-void        print_list_32(t_list *list, int obj)
+void		print_list_32(t_list *list, int obj)
 {
-	t_nm_basic      *objects;
-	char            tp;
+	t_nm_basic		*objects;
+	char			tp;
 
 	if (!list)
 		return ;
 	while (list)
 	{
-		objects = (t_nm_basic *) list->content;
+		objects = (t_nm_basic *)list->content;
 		if (obj)
 			tp = get_type_o(objects);
 		else
@@ -57,7 +57,7 @@ void        print_list_32(t_list *list, int obj)
 		if (ft_strlen(objects->name) > 0 && tp != '\0')
 		{
 			if (objects->value != 0)
-				ft_printf("%08llx %c %s\n", objects->value, tp , objects->name);
+				ft_printf("%08llx %c %s\n", objects->value, tp, objects->name);
 			else
 				ft_printf("%8s %c %s\n", "", tp, objects->name);
 		}
@@ -65,7 +65,7 @@ void        print_list_32(t_list *list, int obj)
 	}
 }
 
-void        set_type(char *n_type, t_type *type)
+void		set_type(char *n_type, t_type *type)
 {
 	type->n_ext = 0x01 & *n_type;
 	type->n_type = 0x0e & *n_type;
@@ -73,13 +73,13 @@ void        set_type(char *n_type, t_type *type)
 	type->n_stab = 0xe0 & *n_type;
 }
 
-void        build_list(struct symtab_command *sym, char *ptr, int obj)
+void		build_list(struct symtab_command *sym, char *ptr, int obj)
 {
-	uint32_t            i;
-	struct nlist_64     *arr;
-	char                *s_table;
-	t_list              *objects;
-	t_nm_basic          *object;
+	uint32_t			i;
+	struct nlist_64		*arr;
+	char				*s_table;
+	t_list				*objects;
+	t_nm_basic			*object;
 
 	i = 0;
 	s_table = (void *)ptr + sym->stroff;
@@ -100,16 +100,16 @@ void        build_list(struct symtab_command *sym, char *ptr, int obj)
 	print_list(objects, obj);
 }
 
-void        print_list(t_list *list, int obj)
+void		print_list(t_list *list, int obj)
 {
-	t_nm_basic      *objects;
-	char            tp;
+	t_nm_basic		*objects;
+	char			tp;
 
 	if (!list)
 		return ;
 	while (list)
 	{
-		objects = (t_nm_basic *) list->content;
+		objects = (t_nm_basic *)list->content;
 		if (obj)
 			tp = get_type_o(objects);
 		else
@@ -117,7 +117,7 @@ void        print_list(t_list *list, int obj)
 		if (ft_strlen(objects->name) > 0 && tp != '\0')
 		{
 			if (tp != 'U')
-				ft_printf("%016llx %c %s\n", objects->value, tp , objects->name);
+				ft_printf("%016llx %c %s\n", objects->value, tp, objects->name);
 			else
 				ft_printf("%16s %c %s\n", "", tp, objects->name);
 		}
